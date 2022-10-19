b491c5d62791261d554fb4e35e37ed2f4f0c5144
ango3636 <90883456+ango3636@users.noreply.github.com>
2022-10-19 18:20:28 -0500

    Add files via upload

---
+++ Exception Handling/function.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <stdexcept>
+using namespace std;
+
+int check_num(int* array, int num_input, int file_size){
+  for (int i=0; i < file_size; i++){
+    if (num_input != array[i]){
+      continue;
+    }
+    else{
+      return i;
+    }
+    }
+  return -1;
+}
+
+int modify_num(int*& array, int index){
+  //search array, find index of old value, return new and old value
+  int new_value;
+  try{
+    cin >> new_value;
+    if (cin.fail()){
+      throw 1;
+    }
+    }
+  catch (int input_error){
+    cin.clear();
+    cin.ignore(numeric_limits<streamsize>::max(),'\n');
+    cout << "Invalid input: Must enter an integer" << endl;
+  }
+  array[index] = new_value;
+  return new_value;
+}
+
+void add_num(int*& array, int num_input, int& file_size){
+  //add num to end of the array
+  file_size++;
+  array[file_size] = num_input;
+}
+
+void remove_num(int*& array, int index, int& file_size){
+  //takes index, removes number at that index
+  int delete_this = array[index];
+  for (int i=0;i < file_size; i++){
+    if(array[i]==delete_this){
+      for(int j=i; j<(file_size-1); j++){
+        array[j]=array[j+1];
+      }
+      break;
+    }
+  }
+  file_size--;
+}
+
+char print_menu(){
+  char choice;
+  cout << "Choose an option:" << endl;
+  cout << "(a) Check if your number is in the file" << endl;
+  cout << "(b) Modify a number by index" << endl;
+  cout << "(c) Add an integer" << endl;
+  cout << "(d) Remove an integer by index" << endl;
+  cout << "(e) Quit" << endl;
+  cout << "(f) Show array" << endl;
+  cin >> choice;
+  return choice;
+}
