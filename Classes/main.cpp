3277c95e713d284bbbc001e407fc6d903c177e17
ango3636 <90883456+ango3636@users.noreply.github.com>
2022-10-21 21:54:22 -0500

    Add files via upload

---
+++ Classes/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+#include <stdexcept>
+using namespace std;
+
+int main() {
+  Professional employee1;
+  Nonprofessional employee2;
+
+int x;
+  cout << "\nHello user!" << "\nAre you a Professional or NonProfessional Employee?" << "\nIf Pro, Enter 1, if Non-Pro, Enter 2: ";
+  cin >> x;
+  cout << endl << endl;
+  if(x == 1){
+    employee1.input();
+    employee1.output();
+  }
+  else if(x== 2){
+    employee2.input();
+    employee2.output();
+  }
+}
